9f00ae0663b7c742d7d2ace4494b1da4ff17b7ed
WhiteHatKid <64671039+WhiteHatKid@users.noreply.github.com>
2021-03-31 16:14:38 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -237,9 +237,9 @@ function draw() {
   }
 
   fill(255, 255, 0, 100);
-  strokeWeight(10);
+  strokeWeight(canArea/300000);
   stroke(255, 255, 0);
-  touchCircle = ellipse(mouseX, mouseY, 100);
+  touchCircle = ellipse(mouseX, mouseY, canArea/30000);
 
   if(player.y < canH / 1.15 && SD)
   player.y += playerYSpeed - (canH / 40);
